8f5acec11c5623d1b6af424d3f89576e7390acf6
Eubash <ssimplatina@gmial.com>
2017-07-09 17:04:04 +0200

    change db with postgress

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'djangogirls',
+            'USER': 'eubash',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
 }
 
 
